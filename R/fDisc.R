7ce8e725a54cdcca1b48c9876485b55a99e09dec
JPantaleon <58693150+jezreel-me@users.noreply.github.com>
2020-09-07 02:46:43 -0500

    Updated functions

--- R/fDisc.R
+++ R/fDisc.R
@@ -12,21 +12,21 @@
 fDisc <- function(final.results, l2fc.co, pval.co, pval.sel) {
   # Label features with by DEM per group using pval
   DEMs.pval <- data.frame(
-    A.DEM = ifelse (abs(final.results$d.facA) >= l2fc.co & final.results$pv.facA <= pval.co,"Yes", "No"),
-    B.DEM = ifelse (abs(final.results$d.facB) >= l2fc.co & final.results$pv.facB <= pval.co,"Yes", "No"),
-    AB.DEM = ifelse (abs(final.results$d.facAB) >= l2fc.co & final.results$pv.facAB <= pval.co, "Yes", "No"))
+    A.DEM = ifelse (abs(final.results$d.facA) >= l2fc.co & final.results$pval.exp.facA <= pval.co,"Yes", "No"),
+    B.DEM = ifelse (abs(final.results$d.facB) >= l2fc.co & final.results$pval.exp.facB <= pval.co,"Yes", "No"),
+    AB.DEM = ifelse (abs(final.results$d.facAB) >= l2fc.co & final.results$pval.exp.facAB <= pval.co, "Yes", "No"))
 
   # Label features with by DEM per group using BH
   DEMs.BH <- data.frame(
-    A.DEM = ifelse (abs(final.results$d.facA) >= l2fc.co & final.results$adjpvBH.facA <= pval.co,"Yes", "No"),
-    B.DEM = ifelse (abs(final.results$d.facB) >= l2fc.co & final.results$adjpvBH.facB <= pval.co,"Yes", "No"),
-    AB.DEM = ifelse (abs(final.results$d.facAB) >= l2fc.co & final.results$adjpvBH.facAB <= pval.co, "Yes", "No"))
+    A.DEM = ifelse (abs(final.results$d.facA) >= l2fc.co & final.results$adjpvBH.exp.facA <= pval.co,"Yes", "No"),
+    B.DEM = ifelse (abs(final.results$d.facB) >= l2fc.co & final.results$adjpvBH.exp.facB <= pval.co,"Yes", "No"),
+    AB.DEM = ifelse (abs(final.results$d.facAB) >= l2fc.co & final.results$adjpvBH.exp.facAB <= pval.co, "Yes", "No"))
 
   # Label features with by DEM per group using Bonf
   DEMs.Bonf <- data.frame(
-    A.DEM = ifelse (abs(final.results$d.facA) >= l2fc.co & final.results$adjpvBonf.facA <= pval.co,"Yes", "No"),
-    B.DEM = ifelse (abs(final.results$d.facB) >= l2fc.co & final.results$adjpvBonf.facB <= pval.co,"Yes", "No"),
-    AB.DEM = ifelse (abs(final.results$d.facAB) >= l2fc.co & final.results$adjpvBonf.facAB <= pval.co, "Yes", "No"))
+    A.DEM = ifelse (abs(final.results$d.facA) >= l2fc.co & final.results$adjpvBonf.exp.facA <= pval.co,"Yes", "No"),
+    B.DEM = ifelse (abs(final.results$d.facB) >= l2fc.co & final.results$adjpvBonf.exp.facB <= pval.co,"Yes", "No"),
+    AB.DEM = ifelse (abs(final.results$d.facAB) >= l2fc.co & final.results$adjpvBonf.exp.facAB <= pval.co, "Yes", "No"))
 
   # Error in pval.sel
   DEMs.Err <- data.frame(Err.DEM = paste("Err.DEM.", seq(1:nrow(final.results)), sep = ""))
@@ -38,11 +38,11 @@ fDisc <- function(final.results, l2fc.co, pval.co, pval.sel) {
                                                  ifelse(pval.sel == "Bonf", DEMs.Bonf, DEMs.Err))))
 
   # Label your AB.DEM by factorial effects
-  final.labels$AB.SME.A <- ifelse (final.labels$AB.DEM == "Yes" & final.labels$pv.SME.facA <= 0.05,
+  final.labels$AB.SME.A <- ifelse (final.labels$AB.DEM == "Yes" & final.labels$pval.con.SME.facA <= 0.05,
                                    "Yes", "No")
-  final.labels$AB.SME.B <- ifelse (final.labels$AB.DEM == "Yes" & final.labels$pv.SME.facB <= 0.05,
+  final.labels$AB.SME.B <- ifelse (final.labels$AB.DEM == "Yes" & final.labels$pval.con.SME.facB <= 0.05,
                                    "Yes", "No")
-  final.labels$AB.iDEM <- ifelse (final.labels$AB.DEM == "Yes" & final.labels$pv.Inter.fAxfB <= 0.05,
+  final.labels$AB.iDEM <- ifelse (final.labels$AB.DEM == "Yes" & final.labels$pval.con.Inter.fAxfB <= 0.05,
                                   "Yes", "No")
 
   # Label your AB.DEM by expression profiles
--- R/samHC.R
+++ R/samHC.R
@@ -6,9 +6,10 @@
 #' @return Text
 #' @export
 #'
-#' @import stats ClassDiscovery rafalib
+#' @import stats ClassDiscovery rafalib grDevices
 samHC <- function(dataset, col.samples) {
   hcl.data <- stats::hclust(ClassDiscovery::distanceMatrix(dataset =  dataset, metric =  "pearson"), method = "ward.D2")
   qc.hcl.data <- rafalib::myplclust(hcl.data, labels = hcl.data$labels, lab.col = col.samples, main = "Sample Hierarchical Clustering", sub = "Review for potential outliers")
-  return(qc.hcl.data)
+  true.qc.hcl <- grDevices::recordPlot()
+  return(true.qc.hcl)
 }
