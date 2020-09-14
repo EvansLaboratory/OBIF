28b3dc706c56183a290687e95c2bb6fae12a5046
Pantaleon Garcia <JPantaleon@mdanderson.org>
2020-09-14 13:17:11 -0500

    Updated function definitions.

--- R/build.R
+++ R/build.R
@@ -1,12 +1,14 @@
-#' Title
+#' Build function
 #'
-#' @param featIDs Text
-#' @param meta.data Text
-#' @param dataAR Text
-#' @param obffa.res Text
-#' @param synex.res Text
+#' This function is used to finalize the Omics Screening component of OBIF, and prepares a complete dataset that will be used for the Feature Discovery component of OBIF.
 #'
-#' @return Text
+#' @param featIDs A vector object containing the unique IDs or names of the features of the Omics dataset
+#' @param meta.data A data frame containing the additional metadata to be associated with each feature
+#' @param dataAR A data frame containing the OBIF-scaled analysis ready expression values of all features per samples
+#' @param obffa.res A data frame with all, or selected, results from the Full Factorial Analysis
+#' @param synex.res A data frame with all, or selected, results from the relative expression calculations and scorings
+#'
+#' @return A data frame containing the merged columns of all the inputs. This dataset is to be used for Feature Discovery analysis.
 #' @export
 #'
 build <- function(featIDs, meta.data, dataAR, obffa.res, synex.res) {
--- R/datAR.R
+++ R/datAR.R
@@ -1,14 +1,17 @@
-#' Title
+#' DatAR function
 #'
-#' @param Data Text
-#' @param colnum1 Text
-#' @param colnum2 Text
-#' @param n.ctrl Text
-#' @param n.facA Text
-#' @param n.facB Text
-#' @param n.facAB Text
+#' This function extracts the data matrix m from an original data object. The data matrix m contains the expression values of all the features, as rows, from all the samples, as columns, as ordered for OBIF's input.
+#' Hence, the data object "Data" must be specified including the first, "colnum1", and last, "colnum2, column number of the data matrix m within the original data.
 #'
-#' @return Text
+#' @param Data A data frame containing the original Omics data being studied by OBIF. As minimum components, it must include the data matrix m, and additional columns can include feature names or metadata as needed.
+#' @param colnum1 A numeric value indicating the column number where the data matrix m starts. That is the column of the first unexposed sample.
+#' @param colnum2 A numeric value indicating the column number where the data matrix m ends. That is the column of the last combined exposed sample.
+#' @param n.ctrl A numeric value indicating the number of columns from the unexposed, control, samples in the data matrix m.
+#' @param n.facA A numeric value indicating the number of columns from the single factor A exposed samples in the data matrix m.
+#' @param n.facB A numeric value indicating the number of columns from the single factor B exposed samples in the data matrix m.
+#' @param n.facAB A numeric value indicating the number of columns from the combined factor A + B exposed samples in the data matrix m.
+#'
+#' @return A data frame containing the unscaled original values of the data matrix m that will be used for analysis
 #' @export
 #'
 #'
